35f0c07074f2696f011ababd176d8a3d8a7d0b14
jayamirudin <50808338+jayamirudin@users.noreply.github.com>
2019-06-10 01:58:33 +0700

    up

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+
+$data['router'] = array ('1'=>'router!10.10.0.12','router@|@coeg','router#|#nKCXq6SXpGNjcQ==','router%coeg','router^coeg.net','router&Rp','router*10','router(1','router)0','router=0','router@!@enable');
